5f47f7f996c99433b506eabaac668483c2636401
Akash <asheelavant4@gmail.com>
2023-01-05 12:06:59 +0530

    Accessing context using Consumer.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,17 +4,21 @@ import { ThemeContext } from '../contexts/ThemeContext'
 class BookList extends Component {
     static contextType = ThemeContext;
     render() {
-        const { isLightTheme, light, dark } = this.context
-        const theme = isLightTheme ? light : dark
-        return (
-            <div className="book-list" style={{color: theme.syntax, background: theme.bg}}>
-                <ul>
-                    <li style={{background: theme.ui}}>Book 1</li>
-                    <li style={{background: theme.ui}}>Book 2</li>
-                    <li style={{background: theme.ui}}>Book 3</li>
-                </ul>
-            </div>
-        );
+        return(
+            <ThemeContext.Consumer>{(context) => {                
+                const { isLightTheme, light, dark } = context
+                const theme = isLightTheme ? light : dark
+                return (
+                    <div className="book-list" style={{color: theme.syntax, background: theme.bg}}>
+                        <ul>
+                            <li style={{background: theme.ui}}>Book 1</li>
+                            <li style={{background: theme.ui}}>Book 2</li>
+                            <li style={{background: theme.ui}}>Book 3</li>
+                        </ul>
+                    </div>
+        )
+            }}</ThemeContext.Consumer>
+        );                
     }
 }
 
