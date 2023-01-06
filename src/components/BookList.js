0aa0e636c3d98aac213263b7e66c16d13060489a
Akash <asheelavant4@gmail.com>
2023-01-06 16:41:44 +0530

    Using hook to read context.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
-class BookList extends Component {
-    static contextType = ThemeContext;
-    render() {
-        return(
-            <ThemeContext.Consumer>{(context) => {                
-                const { isLightTheme, light, dark } = context
-                const theme = isLightTheme ? light : dark
-                return (
-                    <div className="book-list" style={{color: theme.syntax, background: theme.bg}}>
-                        <ul>
-                            <li style={{background: theme.ui}}>Book 1</li>
-                            <li style={{background: theme.ui}}>Book 2</li>
-                            <li style={{background: theme.ui}}>Book 3</li>
-                        </ul>
-                    </div>
-        )
-            }}</ThemeContext.Consumer>
-        );                
-    }
+const BookList = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext)
+    const theme = isLightTheme ? light : dark
+    return (
+        <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+            <ul>
+                <li style={{ background: theme.ui }}>Book 1</li>
+                <li style={{ background: theme.ui }}>Book 2</li>
+                <li style={{ background: theme.ui }}>Book 3</li>
+            </ul>
+        </div>
+    );
 }
 
 export default BookList;
